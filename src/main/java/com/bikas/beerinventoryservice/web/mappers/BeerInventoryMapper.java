c54d2772838a79b40db4dcf27166c5837cd0cb24
Bikas Kumar Patro <bikas1986@gmail.com>
2020-04-20 22:26:32 +0530

    Initial project setup

---
+++ src/main/java/com/bikas/beerinventoryservice/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,13 @@
+package com.bikas.beerinventoryservice.web.mappers;
+
+import com.bikas.beerinventoryservice.domain.BeerInventory;
+import com.bikas.beerinventoryservice.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
---
+++ src/test/java/com/bikas/beerinventoryservice/BeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikas.beerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
